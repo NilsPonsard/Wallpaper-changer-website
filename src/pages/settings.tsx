11019445c53f4fb890115d3cd902ca9f224110f9
Nils Ponsard <nilsponsard@gmail.com>
2022-10-02 19:15:43 +0200

    fix: logout

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,26 +1,30 @@
 import { Box, Typography, Button } from '@mui/material';
+import { useRouter } from 'next/router';
 import { useSnackbar } from 'notistack';
-import React from 'react';  
+import React from 'react';
 import TokenPanel from '../components/Token';
 import { logout } from '../lib/api/api';
 import { useLoginContext } from '../lib/loginContext';
 
 export default function Settings() {
-  
-    const {credentialsManager} = useLoginContext();
-    const { enqueueSnackbar } = useSnackbar();
-  
+  const { credentialsManager, validUser } = useLoginContext();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const router = useRouter();
+
   const handleLogout = () => {
     logout(credentialsManager).then(() => {
       enqueueSnackbar('Logged out', { variant: 'success' });
+      router.push('/');
     });
   };
+
+  if (!validUser) router.push('/');
 
   return (
     <Box>
       <Typography variant="h3">Settings</Typography>
       <TokenPanel />
-
 
       <Button onClick={handleLogout} variant="contained">
         Logout
